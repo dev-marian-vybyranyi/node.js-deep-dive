85fdef53f42287f2160bc7fc1ca83597b26c9734
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-23 15:28:16 +0200

    add writing posibility saving user masages to array implement uI features

--- networking/chat-app/client.js
+++ networking/chat-app/client.js
@@ -1,9 +1,62 @@
 const net = require("net");
+const readline = require("readline/promises");
 
-const client = net.createConnection({ host: "127.0.0.1", port: 3008 }, () => {
-  console.log("Connected to the server!");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
 });
 
-client.on("end", () => {
+const clearLine = (dir) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.clearLine(dir, () => {
+      resolve();
+    });
+  });
+};
+
+const moveCursor = (dx, dy) => {
+  return new Promise((resolve, reject) => {
+    process.stdout.moveCursor(dx, dy, () => {
+      resolve();
+    });
+  });
+};
+
+let id;
+
+const socket = net.createConnection(
+  { host: "127.0.0.1", port: 3008 },
+  async () => {
+    console.log("Connected to the server!");
+
+    const ask = async () => {
+      const message = await rl.question("Enter a message > ");
+      await moveCursor(0, -1);
+      await clearLine(0);
+      socket.write(`${id}-message-${message}`);
+    };
+
+    ask();
+
+    socket.on("data", async (data) => {
+      console.log();
+      await moveCursor(0, -1);
+      await clearLine(0);
+
+      if (data.toString("utf-8").substring(0, 2) === "id") {
+
+        id = data.toString("utf-8").substring(3);
+
+        console.log(`Your id is ${id}!\n`);
+      } else {
+        console.log(data.toString("utf-8"));
+      }
+
+      ask();
+    });
+  }
+);
+
+socket.on("end", () => {
   console.log("Connection was ended!");
 });
