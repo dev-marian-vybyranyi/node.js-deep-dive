95e57793869f2d3440f11cb45b06c4cc97684178
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-26 13:46:51 +0200

    Add login page route and API endpoints for user authentication.

--- http/poster/server.js
+++ http/poster/server.js
@@ -25,6 +25,10 @@ server.route("get", "/", (req, res) => {
   res.sendFile("./public/index.html", "text/html");
 });
 
+server.route("get", "/login", (req, res) => {
+  res.sendFile("./public/index.html", "text/html");
+});
+
 server.route("get", "/styles.css", (req, res) => {
   res.sendFile("./public/styles.css", "text/css");
 });
@@ -34,6 +38,30 @@ server.route("get", "/scripts.js", (req, res) => {
 });
 
 // JSON Routes
+
+server.route("post", "/api/login", (req, res) => {
+  let body = "";
+  req.on("data", (chunk) => {
+    body += chunk.toString("utf-8");
+  });
+
+  req.on("end", () => {
+    body = JSON.parse(body);
+
+    const username = body.username;
+    const password = body.password;
+
+    const user = USERS.find((user) => user.username === username);
+
+    if (user && user.password === password) {
+      res.status(200).json({ message: "Logged in successfully!" });
+    } else {
+      res.status(401).json({ error: "Invalid username or password." });
+    }
+  });
+});
+
+server.route("get", "/api/user", (req, res) => {});
 
 server.route("get", "/api/posts", (req, res) => {
   const posts = POSTS.map((post) => {
