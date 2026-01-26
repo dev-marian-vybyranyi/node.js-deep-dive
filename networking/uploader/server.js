ca0207027659dfdbfca0eba452421d2ebab41e3f
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-26 08:13:13 +0200

    Implement dynamic file naming for file uploads by passing the filename from client to server.

--- networking/uploader/server.js
+++ networking/uploader/server.js
@@ -11,10 +11,14 @@ server.on("connection", (socket) => {
   socket.on("data", async (data) => {
     if (!fileHandle) {
       socket.pause();
-      fileHandle = await fs.open(`storage/test.txt`, "w");
+
+      const indexOfDivider = data.indexOf("-------");
+      const fileName = data.subarray(10, indexOfDivider).toString("utf-8");
+
+      fileHandle = await fs.open(`storage/${fileName}`, "w");
       fileWriteStream = fileHandle.createWriteStream();
 
-      fileWriteStream.write(data);
+      fileWriteStream.write(data.subarray(indexOfDivider + 7));
 
       socket.resume();
       fileWriteStream.on("drain", () => {
@@ -28,7 +32,7 @@ server.on("connection", (socket) => {
   });
 
   socket.on("end", () => {
-    fileHandle.close();
+    if (fileHandle) fileHandle.close();
     fileHandle = undefined;
     fileWriteStream = undefined;
     console.log("Connection ended!");
