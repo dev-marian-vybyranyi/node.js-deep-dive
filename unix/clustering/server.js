d73959e2d80c628c91579a47968cab1fd06ccdf9
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-27 10:53:07 +0200

    Implement Node.js clustering with worker processes to track and handle requests.

--- unix/clustering/server.js
+++ unix/clustering/server.js
@@ -2,11 +2,21 @@ const cpeak = require("cpeak");
 
 const server = new cpeak();
 
+process.on("message", (message) => {
+  console.log(
+    `Worker ${process.pid} received this message from parent: ${message}`
+  );
+});
+
 server.route("get", "/", (req, res) => {
+  process.send({ action: "request" });
+
   res.json({ message: "This is some text." });
 });
 
 server.route("get", "/heavy", (req, res) => {
+  process.send({ action: "request" });
+
   for (let i = 0; i < 10000000000; i++) {}
   res.json({ message: "The operation is now done." });
 });
