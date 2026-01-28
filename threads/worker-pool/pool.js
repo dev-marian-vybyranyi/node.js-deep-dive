f159aab99af1ad15b23420fecff10393d04f93da
dev-marian-vybyranyi <marian.vybyranyi.dev@gmail.com>
2026-01-28 11:56:29 +0200

    Implement a worker pool using `worker_threads` for concurrent prime number generation.

---
+++ threads/worker-pool/pool.js
@@ -0,0 +1,49 @@
+const { Worker } = require("worker_threads");
+
+class Pool {
+  constructor(threadCount) {
+    this.threadCount = threadCount;
+    this.threads = [];
+    this.idleThreads = [];
+    this.scheduledTasks = [];
+    for (let i = 0; i < threadCount; i++) {
+      this.spawnThread();
+    }
+  }
+
+  spawnThread() {
+    const worker = new Worker("./calc.js");
+
+    worker.on("message", (result) => {
+      const { callback } = worker.currentTask;
+
+      if (callback) {
+        callback(result);
+      }
+
+      this.idleThreads.push(worker);
+      this.runNextTask();
+    });
+
+    this.threads.push(worker);
+    this.idleThreads.push(worker);
+  }
+
+  runNextTask() {
+    if (this.scheduledTasks.length > 0 && this.idleThreads.length > 0) {
+      const worker = this.idleThreads.shift();
+      const { taskName, options, callback } = this.scheduledTasks.shift();
+
+      worker.currentTask = { taskName, options, callback };
+
+      worker.postMessage({ taskName, options });
+    }
+  }
+
+  submit(taskName, options, callback) {
+    this.scheduledTasks.push({ taskName, options, callback });
+    this.runNextTask();
+  }
+}
+
+module.exports = Pool;
